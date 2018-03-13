d8baccc416c42c7df2d3cf4a191776bc00879fc8
Zan Fras <zan.fraas@gmail.com>
2018-03-13 12:22:50 +0100

    Basic MVC done

---
+++ Notey/Controllers/CategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryViewController.swift
+//  Notey
+//
+//  Created by Žan Fras on 13/03/2018.
+//  Copyright © 2018 Žan Fras. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Notey/Views/NoteViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  NoteViewCell.swift
+//  Notey
+//
+//  Created by Žan Fras on 13/03/2018.
+//  Copyright © 2018 Žan Fras. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+
+}
