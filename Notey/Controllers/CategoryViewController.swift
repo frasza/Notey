9acbfd86b405ea5cc910d8cb34efe721459f5dcb
Zan Fras <zan.fraas@gmail.com>
2018-03-13 13:37:06 +0100

    Category view controller CoreData saving and deleting works

--- Notey/Controllers/CategoryViewController.swift
+++ Notey/Controllers/CategoryViewController.swift
@@ -7,14 +7,127 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    // Core Data context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Load categories
+        loadCategories()
+    }
+    
+    //MARK: - TableView Datasource Methods
+    /***************************************************************/
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categories.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+        
     }
 
+    
+    //MARK: - TableView Delegate Methods
+    /***************************************************************/
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // Remove category from array and context
+        context.delete(categories[indexPath.row])
+        categories.remove(at: indexPath.row)
+        
+        saveCategories()
+        
+    }
+    
+    
+    //MARK: - Data Manipulation Methods
+    /***************************************************************/
+    
+    // Save to Core Data
+    func saveCategories() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving data, \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    // Load from Core Data
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error loading data, \(error)")
+        }
+        
+    }
+    
+    //MARK: - IBActions
+    /***************************************************************/
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var categoryTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            // Create new category with context
+            let newCategory = Category(context: self.context)
+            newCategory.name = categoryTextField.text!
+            
+            self.categories.append(newCategory)
+            
+            self.saveCategories()
+            
+        }
+        
+        alert.addTextField { (textField) in
+            
+            // Create alert textfield
+            textField.placeholder = "Create new Category"
+            categoryTextField = textField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 
+    
 }
 
